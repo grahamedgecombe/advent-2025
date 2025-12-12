6dffd54c14837b3ebc7adcaff6e2c37519ac7316
Graham Edgecombe <gpe@grahamedgecombe.com>
2025-12-12 19:36:08 +0000

    Add day 11 part 2 solution

--- src/main/kotlin/com/grahamedgecombe/advent2025/day11/Day11.kt
+++ src/main/kotlin/com/grahamedgecombe/advent2025/day11/Day11.kt
@@ -12,19 +12,49 @@ object Day11 : Puzzle<Map<String, List<String>>>(11) {
         }
     }
 
-    override fun solvePart1(input: Map<String, List<String>>): Int {
-        return countPaths(input, "you", emptySet())
+    override fun solvePart1(input: Map<String, List<String>>): Long {
+        return countPaths(input, "you", "out")
     }
 
-    private fun countPaths(graph: Map<String, List<String>>, current: String, seen: Set<String>): Int {
-        if (current == "out") {
+    override fun solvePart2(input: Map<String, List<String>>): Long {
+        val svrToFft = countPaths(input, "svr", "fft")
+        val fftToDac = countPaths(input, "fft", "dac")
+        val dacToOut = countPaths(input, "dac", "out")
+
+        val svrToDac = countPaths(input, "svr", "dac")
+        val dacToFft = countPaths(input, "dac", "fft")
+        val fftToOut = countPaths(input, "fft", "out")
+
+        return (svrToFft * fftToDac * dacToOut) + (svrToDac * dacToFft * fftToOut)
+    }
+
+    private data class Key(
+        val current: String,
+        val goal: String,
+    )
+
+    private fun countPaths(
+        graph: Map<String, List<String>>,
+        current: String,
+        goal: String,
+        cache: MutableMap<Key, Long> = mutableMapOf(),
+    ): Long {
+        if (current == goal) {
             return 1
-        } else if (current in seen) {
-            return 0
         }
 
-        return graph.getOrDefault(current, emptyList()).sumOf { neighbour ->
-            countPaths(graph, neighbour, seen + current)
+        val key = Key(current, goal)
+
+        var count = cache[key]
+        if (count != null) {
+            return count
         }
+
+        count = graph.getOrDefault(current, emptyList()).sumOf { neighbour ->
+            countPaths(graph, neighbour, goal, cache)
+        }
+        cache[key] = count
+
+        return count
     }
 }
--- src/test/kotlin/com/grahamedgecombe/advent2025/Day11Test.kt
+++ src/test/kotlin/com/grahamedgecombe/advent2025/Day11Test.kt
@@ -7,12 +7,18 @@ import kotlin.test.assertEquals
 class Day11Test {
     @Test
     fun testPart1() {
-        assertEquals(5, Day11.solvePart1(TEST_INPUT))
+        assertEquals(5, Day11.solvePart1(TEST_INPUT_1))
         assertEquals(749, Day11.solvePart1(PROD_INPUT))
     }
 
+    @Test
+    fun testPart2() {
+        assertEquals(2, Day11.solvePart2(TEST_INPUT_2))
+        assertEquals(420257875695750, Day11.solvePart2(PROD_INPUT))
+    }
+
     private companion object {
-        private val TEST_INPUT = Day11.parse("""
+        private val TEST_INPUT_1 = Day11.parse("""
             aaa: you hhh
             you: bbb ccc
             bbb: ddd eee
@@ -23,6 +29,21 @@ class Day11Test {
             ggg: out
             hhh: ccc fff iii
             iii: out
+        """.trimIndent())
+        private val TEST_INPUT_2 = Day11.parse("""
+            svr: aaa bbb
+            aaa: fft
+            fft: ccc
+            bbb: tty
+            tty: ccc
+            ccc: ddd eee
+            ddd: hub
+            hub: fff
+            eee: dac
+            dac: fff
+            fff: ggg hhh
+            ggg: out
+            hhh: out
         """.trimIndent())
         private val PROD_INPUT = Day11.parse()
     }
